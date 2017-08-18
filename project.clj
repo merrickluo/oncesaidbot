17b0b20b00969354c33337856eb1c7e6722f6ea9
A.I <merrick@luois.me>
2017-08-18 15:47:46 +0800

    use forked morse

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.imgscalr/imgscalr-lib "4.2"]
-                 [morse "0.2.4"]]
+                 [morse "0.2.5-SNAPSHOT"]]
+  :plugins [[lein-modules "0.3.11"]]
+  :modules {:dirs ["modules/morse"]
+            :subprocess nil}
   :main ^:skip-aot oncesaid.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
